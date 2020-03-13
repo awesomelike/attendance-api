04a45de309aa8a75affcdf5823691c1eb0e599e4
awesomelike <mr.azik98@mail.ru>
2020-03-13 11:50:23 +0500

    removed unnecessary comments

--- util/time.js
+++ util/time.js
@@ -34,21 +34,14 @@ export default {
   },
   async getCurrentWeek() {
     const getWeekNumber = (d) => {
-      // Copy date so don't modify original
+      // eslint-disable-next-line no-param-reassign
       d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
-      // Set to nearest Thursday: current date + 4 - current day number
-      // Make Sunday's day number 7
       d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
-      // Get first day of year
       const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
-      // Calculate full weeks to nearest Thursday
       const weekNo = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
-      // Return array of year and week number
       return weekNo;
     };
     const semesterStartDate = (await Semester.findByPk(2)).startDate;
-    // console.log(`semesterStartDate=${semesterStartDate.getTime()}`);
-    // console.log(`${new Date(2019, 8, 9).getTime() === semesterStartDate.getTime()}`);
     return getWeekNumber(new Date(2019, 8, 27)) - getWeekNumber(semesterStartDate) + 1;
   },
 };
