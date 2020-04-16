b052cb2ac50abee1991b3f39a0eef82c5d32951f
Azamat Abdullaev <mr.azik98@mail.ru>
2020-04-16 19:47:42 +0500

    improved to await

--- controllers/timetable.js
+++ controllers/timetable.js
@@ -56,146 +56,136 @@ const storeTimetable = (req, res) => new Promise((resolve, reject) => {
 
   Promise.all(insertProfessorsAndCourses)
     .then(async () => {
-      const professorsFromDB = await models.Professor.findAll({ attributes: ['id', 'uid'] });
-      const coursesFromDB = await models.Course.findAll();
-      for (let i = 0; i < coursesFromDB.length; i += 1) {
-        const particularSections = timetable
-          .filter((object) => object.courseNumber === coursesFromDB[i].courseNumber);
-        for (let j = 0; j < particularSections.length; j += 1) {
-          const particularClasses = particularSections
-            .filter((section) => particularSections[j].sectionNumber === section.sectionNumber);
-          sections.push({
-            courseId: coursesFromDB[i].id,
-            sectionNumber: particularSections[j].sectionNumber,
-            professorId: professorsFromDB
-              .find((professor) => professor.uid === particularSections[j].professorId).id,
-            semesterId: 2,
-            classes: particularClasses,
+      try {
+        const professorsFromDB = await models.Professor.findAll({ attributes: ['id', 'uid'] });
+        const coursesFromDB = await models.Course.findAll();
+        for (let i = 0; i < coursesFromDB.length; i += 1) {
+          const particularSections = timetable
+            .filter((object) => object.courseNumber === coursesFromDB[i].courseNumber);
+          for (let j = 0; j < particularSections.length; j += 1) {
+            const particularClasses = particularSections
+              .filter((section) => particularSections[j].sectionNumber === section.sectionNumber);
+            sections.push({
+              courseId: coursesFromDB[i].id,
+              sectionNumber: particularSections[j].sectionNumber,
+              professorId: professorsFromDB
+                .find((professor) => professor.uid === particularSections[j].professorId).id,
+              semesterId: 2,
+              classes: particularClasses,
+            });
+          }
+        }
+        const uniqueSections = unique(sections, ['sectionNumber', 'courseId', 'semesterId']);
+
+        const sectionsToInsert = uniqueSections.map(({
+          courseId, sectionNumber, professorId, semesterId,
+        }) => ({
+          courseId, sectionNumber, professorId, semesterId,
+        }));
+
+        // models.sequelize.getQueryInterface().bulkInsert('Sections', sectionsToInsert, {})
+        await models.Section.bulkCreate(sectionsToInsert, { updateOnDuplicate: ['sectionNumber'] });
+        const allSections = await models.Section.findAll({
+          include: [
+            {
+              model: models.Course,
+              as: 'course',
+            },
+          ],
+        });
+        const weekDays = await models.WeekDay.findAll({ raw: true });
+        const dbRooms = await models.Room.findAll({ raw: true });
+        const classesToInsert = [];
+        for (let i = 0; i < uniqueSections.length; i += 1) {
+          const { classes } = uniqueSections[i];
+          const uniqueClasses = unique(classes, ['courseNumber', 'sectionNumber', 'weekDay']);
+          for (let j = 0; j < uniqueClasses.length; j += 1) {
+            classesToInsert.push({
+              sectionId: allSections
+                .find((section) => section.sectionNumber === uniqueClasses[j].sectionNumber
+                  && section.course.courseNumber === uniqueClasses[j].courseNumber).id,
+              weekDayId: weekDays
+                .find((weekDay) => weekDay.key === uniqueClasses[j].weekDay).id,
+              roomId: dbRooms.find(({ label }) => label === uniqueClasses[j].room).id,
+            });
+          }
+        }
+        // models.sequelize.getQueryInterface().bulkInsert('Classes', classesToInsert)
+        await models.Class.bulkCreate(classesToInsert, { updateOnDuplicate: ['sectionId', 'roomId', 'weekDayId'] });
+        const dbClasses = await models.Class.findAll({
+          include: [
+            {
+              model: models.Section,
+              as: 'section',
+              include: [
+                {
+                  model: models.Professor,
+                  as: 'professor',
+                },
+                {
+                  model: models.Course,
+                  as: 'course',
+                },
+              ],
+            },
+          ],
+        });
+        const timeSlots = await models.TimeSlot.findAll({ raw: true });
+        const classTimeSlots = [];
+        for (let i = 0; i < uniqueSections.length; i += 1) {
+          const { classes } = uniqueSections[i];
+          classes.forEach((inclass) => {
+            const classId = dbClasses
+              .find((obj) => obj.weekDayId === weekDays
+                .find((wd) => wd.key === inclass.weekDay).id
+                      && obj.section.course.courseNumber === inclass.courseNumber
+                      && obj.section.sectionNumber === inclass.sectionNumber).id;
+            const timeSlotId = timeSlots
+              .find((timeslot) => timeslot.startTime === inclass.timeslot).id;
+            classTimeSlots.push({ timeSlotId, classId });
           });
         }
-      }
-      const uniqueSections = unique(sections, ['sectionNumber', 'courseId', 'semesterId']);
-
-      const sectionsToInsert = uniqueSections.map(({
-        courseId, sectionNumber, professorId, semesterId,
-      }) => ({
-        courseId, sectionNumber, professorId, semesterId,
-      }));
-
-      // models.sequelize.getQueryInterface().bulkInsert('Sections', sectionsToInsert, {})
-      models.Section.bulkCreate(sectionsToInsert, { updateOnDuplicate: ['sectionNumber'] })
-        .then(async () => {
-          const allSections = await models.Section.findAll({
-            include: [
-              {
-                model: models.Course,
-                as: 'course',
-              },
-            ],
-          });
-          const weekDays = await models.WeekDay.findAll({ raw: true });
-          const dbRooms = await models.Room.findAll({ raw: true });
-          const classesToInsert = [];
-          for (let i = 0; i < uniqueSections.length; i += 1) {
-            const { classes } = uniqueSections[i];
-            const uniqueClasses = unique(classes, ['courseNumber', 'sectionNumber', 'weekDay']);
-            for (let j = 0; j < uniqueClasses.length; j += 1) {
-              classesToInsert.push({
-                sectionId: allSections
-                  .find((section) => section.sectionNumber === uniqueClasses[j].sectionNumber
-                  && section.course.courseNumber === uniqueClasses[j].courseNumber).id,
-                weekDayId: weekDays
-                  .find((weekDay) => weekDay.key === uniqueClasses[j].weekDay).id,
-                roomId: dbRooms.find(({ label }) => label === uniqueClasses[j].room).id,
+        await models.ClassTimeSlot.bulkCreate(classTimeSlots, { updateOnDuplicate: ['timeSlotId', 'classId'] });
+        const classItems = [];
+        dbClasses.forEach((dbClass) => {
+          for (let i = 1; i <= 16; i += 1) {
+            if (i !== 8 && i !== 16) {
+              classItems.push({
+                classId: dbClass.id,
+                week: i,
+                classItemStatusId: 1,
               });
             }
           }
-          // models.sequelize.getQueryInterface().bulkInsert('Classes', classesToInsert)
-          models.Class.bulkCreate(classesToInsert, { updateOnDuplicate: ['sectionId', 'roomId', 'weekDayId'] })
-            .then(async () => {
-              const dbClasses = await models.Class.findAll({
-                include: [
-                  {
-                    model: models.Section,
-                    as: 'section',
-                    include: [
-                      {
-                        model: models.Professor,
-                        as: 'professor',
-                      },
-                      {
-                        model: models.Course,
-                        as: 'course',
-                      },
-                    ],
-                  },
-                ],
-              });
-              const timeSlots = await models.TimeSlot.findAll({ raw: true });
-              const classTimeSlots = [];
-              for (let i = 0; i < uniqueSections.length; i += 1) {
-                const { classes } = uniqueSections[i];
-                classes.forEach((inclass) => {
-                  const classId = dbClasses
-                    .find((obj) => obj.weekDayId === weekDays
-                      .find((wd) => wd.key === inclass.weekDay).id
-                      && obj.section.course.courseNumber === inclass.courseNumber
-                      && obj.section.sectionNumber === inclass.sectionNumber).id;
-                  const timeSlotId = timeSlots
-                    .find((timeslot) => timeslot.startTime === inclass.timeslot).id;
-                  classTimeSlots.push({ timeSlotId, classId });
-                });
-              }
-              models.ClassTimeSlot.bulkCreate(classTimeSlots, { updateOnDuplicate: ['timeSlotId', 'classId'] })
-                .then(() => {
-                  const classItems = [];
-                  dbClasses.forEach((dbClass) => {
-                    for (let i = 1; i <= 16; i += 1) {
-                      if (i !== 8 && i !== 16) {
-                        classItems.push({
-                          classId: dbClass.id,
-                          week: i,
-                          classItemStatusId: 1,
-                        });
-                      }
-                    }
-                  });
-                  // models.sequelize.getQueryInterface().bulkInsert('ClassItems', classItems, {})
-                  models.ClassItem.bulkCreate(classItems, { updateOnDuplicate: ['plannedDate', 'classId', 'week'] })
-                    .then(async () => {
-                      const students = await models.Student.findAll();
-                      const dbSections = await models.Section.findAll();
-                      const dbCourses = await models.Course.findAll();
-                      const studentSections = [];
-                      for (let i = 0; i < students.length; i += 1) {
-                        const particularStudents = studentsTimetable
-                          .filter((st) => st['Student ID'] === students[i].uid);
-                        for (let j = 0; j < particularStudents.length; j += 1) {
-                          studentSections.push({
-                            studentId: students.find((student) => particularStudents[j]['Student ID'] === student.uid).id,
-                            sectionId: dbSections
-                              .find((section) => section.courseId === dbCourses
-                                .find((course) => course.courseNumber === particularStudents[j]['Course No']).id
+        });
+        // models.sequelize.getQueryInterface().bulkInsert('ClassItems', classItems, {})
+        await models.ClassItem.bulkCreate(classItems, { updateOnDuplicate: ['plannedDate', 'classId', 'week'] });
+        const students = await models.Student.findAll();
+        const dbSections = await models.Section.findAll();
+        const dbCourses = await models.Course.findAll();
+        const studentSections = [];
+        for (let i = 0; i < students.length; i += 1) {
+          const particularStudents = studentsTimetable
+            .filter((st) => st['Student ID'] === students[i].uid);
+          for (let j = 0; j < particularStudents.length; j += 1) {
+            studentSections.push({
+              studentId: students.find((student) => particularStudents[j]['Student ID'] === student.uid).id,
+              sectionId: dbSections
+                .find((section) => section.courseId === dbCourses
+                  .find((course) => course.courseNumber === particularStudents[j]['Course No']).id
                                 && section.sectionNumber === particularStudents[j]['Class No']).id,
-                          });
-                        }
-                      }
-                      // models.sequelize.getQueryInterface().bulkInsert('StudentSections', studentSections, {})
-                      models.StudentSection.bulkCreate(studentSections, { updateOnDuplicate: ['studentId', 'sectionId'] })
-                        .then(async () => {
-                          resolve('success');
-                        })
-                        .catch((error) => reject(error));
-                    });
-                })
-                .catch((error) => res.status(502).json(error));
             });
-        })
-        .catch((error) => res.status(502).json(error));
+          }
+        }
+        // models.sequelize.getQueryInterface().bulkInsert('StudentSections', studentSections, {})
+        await models.StudentSection.bulkCreate(studentSections, { updateOnDuplicate: ['studentId', 'sectionId'] });
+      } catch (error) {
+        res.status(502).json(error);
+      }
     });
 });
 
-const storeStudents = () => {
+const storeStudents = async () => {
   const students = [];
   studentsTimetable.forEach((obj) => {
     students.push({
@@ -209,20 +199,18 @@ const storeStudents = () => {
 
   const uniqueStudents = Array.from(new Set(students.map((student) => student.uid)))
     .map((uid) => students.find((student) => student.uid === uid));
-  models.Student.bulkCreate(uniqueStudents, { returning: true, updateOnDuplicate: ['name'] })
-    .then((results) => {
-      const telegramAccounts = [];
-      console.log('Students inserted');
-      mobiles.forEach((mobile) => {
-        telegramAccounts.push({
-          studentId: results.find(({ uid }) => uid === mobile.uid).id,
-          phoneNumber: mobile.phoneNumber,
-        });
-      });
-      // models.sequelize.getQueryInterface().bulkInsert('TelegramAccounts', telegramAccounts, {})
-      models.TelegramAccount.bulkCreate(telegramAccounts)
-        .then(() => console.log('Telegram accounts inserted'));
+  const results = await models.Student.bulkCreate(uniqueStudents, { returning: true, updateOnDuplicate: ['name'] });
+  const telegramAccounts = [];
+  console.log('Students inserted');
+  mobiles.forEach((mobile) => {
+    telegramAccounts.push({
+      studentId: results.find(({ uid }) => uid === mobile.uid).id,
+      phoneNumber: mobile.phoneNumber,
     });
+  });
+  // models.sequelize.getQueryInterface().bulkInsert('TelegramAccounts', telegramAccounts, {})
+  await models.TelegramAccount.bulkCreate(telegramAccounts);
+  console.log('Telegram accounts inserted!');
 };
 
 const insertDummyRecords = async (req, res) => {
